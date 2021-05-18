bef00d22ca3773c66fd47b3d472f50a9d372247b
Lailin Xu <xlltoade@gmail.com>
2021-05-18 17:25:56 +0800

    up

---
+++ Fitting/rpc_reso_fit3.C
@@ -0,0 +1,344 @@
+/// \file
+/// \ingroup tutorial_fit
+/// \notebook
+/// example of fitting a 3D function
+///
+/// \macro_output
+/// \macro_code
+///
+/// \author Lailin Xu
+
+
+#include "TROOT.h"
+#include "TStyle.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "TFile.h"
+#include "TF3.h"
+#include "TH3F.h"
+#include "TError.h"
+#include "TMatrixD.h"
+#include "TMath.h"
+#include "Math/MinimizerOptions.h"
+#include "Math/GenAlgoOptions.h"
+#include "Fit/BinData.h"
+#include "Fit/Fitter.h"
+#include "Math/WrappedMultiTF1.h"
+
+// Helper function to print a matrx(m, n)
+void print_matrix(TMatrixD& m_cov, int md, int nd) {
+
+  for(int j=0; j < md; j++) {
+    for(int k=0; k < md; k++) {
+      std::cout << j << " " << k << " " << TMatrixDRow(m_cov, j)[k] << std::endl;
+    }
+  }
+}
+
+// Helper function for 3D Gaussian
+Double_t gaus_3d(Double_t *x, Double_t *par) {
+
+  // 3D Gaussian
+  const int nd = 3;
+
+  // Mean
+  // x1-x2, x1-x3, x2-x3
+  Double_t mean[nd] = {par[0], par[1], par[2]};
+
+  // Covariance matrix 
+  //  sig1^2+sig2^2, sig1^2, sig2^2
+  //  sig1^2, sig1^2+sig3^2, sig3^2
+  //  sig2^2, sig3^2, sig2^2+sig3^2
+  TMatrixD cov(nd, nd);
+  TMatrixDRow(cov, 0)[0] = TMath::Power(par[3], 2) + TMath::Power(par[4], 2);
+  TMatrixDRow(cov, 0)[1] = TMath::Power(par[3], 2);
+  TMatrixDRow(cov, 0)[2] = TMath::Power(par[4], 2);
+  TMatrixDRow(cov, 1)[0] = TMath::Power(par[3], 2);
+  TMatrixDRow(cov, 1)[1] = TMath::Power(par[3], 2) + TMath::Power(par[5], 2);
+  TMatrixDRow(cov, 1)[2] = TMath::Power(par[5], 2);
+  TMatrixDRow(cov, 2)[0] = TMath::Power(par[4], 2);
+  TMatrixDRow(cov, 2)[1] = TMath::Power(par[5], 2);
+  TMatrixDRow(cov, 2)[2] = TMath::Power(par[4], 2) + TMath::Power(par[5], 2);
+
+  // Build the multi-Gaussian
+  TMatrixD m_x(nd, 1);
+  for(int i=0; i<nd; i++) {
+    TMatrixDRow(m_x, i)[0] = x[i] - mean[i];
+  }
+
+  TMatrixD m_x_t(m_x);
+  m_x_t.T();
+  
+  Double_t cov_dr[nd];
+  TMatrixD icov(cov);
+  icov.Invert(cov_dr);
+  Double_t cov_d = cov.Determinant();
+  Double_t icov_d = icov.Determinant();
+
+  Double_t r = 0;
+  for(int i=0; i<nd; i++) {
+    for(int j=0; j<nd; j++) {
+      r += TMatrixDRow(m_x_t, 0)[j] * TMatrixDRow(icov, i)[j] * TMatrixDRow(m_x, i)[0];
+    }
+  }
+
+  Double_t y = 1./(TMath::Power(2*TMath::Pi(), nd/2.) * TMath::Sqrt(cov_d)) * TMath::Exp( -0.5* r ) ;
+
+  return y;
+}
+
+void rpc_reso_fit3() {
+  gROOT->SetStyle("ATLAS");
+  gStyle->SetPalette(1);
+
+  TString pname = "gaus_3D_test_C3";
+  TFile *tfout = new TFile(pname + ".root", "RECREATE");
+
+  const int nd = 3;
+  
+  // Plotting
+  // =====================
+  TCanvas *myc = new TCanvas("c", "c", 800, 600);
+  myc->SetFillColor(0);
+  
+  int nbinsx=60, nbinsy=60, nbinsz=60;
+  float xmin=-3, xmax=3;
+  float ymin=-3, ymax=3;
+  float zmin=-3, zmax=3;
+  
+  // Overlay the multi-Gaussian PDF
+  
+  
+  // Fit
+  int npar = 6; // mean: 3; sigma: 3
+  TF3 *g_s = new TF3("gaus_s", gaus_3d, xmin, xmax, ymin, ymax, zmin, zmax, npar);
+  g_s->SetParameter(0, 0.);
+  g_s->SetParameter(1, 0.);
+  g_s->SetParameter(2, 0.);
+  g_s->SetParameter(3, 1.);
+  g_s->SetParameter(4, 0.8);
+  g_s->SetParameter(5, 1.2);
+  
+  TString hname = "3D";
+  TH3F *h_s = new TH3F(hname, hname, nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax);
+  const int nevents = 1000;
+  h_s->FillRandom("gaus_s", nevents);
+  
+  h_s->GetXaxis()->SetRangeUser(-3, 3);
+  h_s->GetYaxis()->SetRangeUser(-3, 3);
+  h_s->GetZaxis()->SetRangeUser(-3, 3);
+  h_s->GetXaxis()->SetTitle("X1");
+  h_s->GetYaxis()->SetTitle("X2");
+  h_s->GetZaxis()->SetTitle("X3");
+  
+  h_s->Draw("colz");
+  
+  myc->Draw();
+  myc->SaveAs(pname+"_1.png");
+  
+  // Projection
+  TH1F *hx = (TH1F*)h_s->ProjectionX();
+  TH1F *hy = (TH1F*)h_s->ProjectionY();
+  TH1F *hz = (TH1F*)h_s->ProjectionZ();
+  
+  // X-axis
+  myc->Clear();
+  hx->Draw();
+  myc->Draw();
+  myc->SaveAs(pname+"_1_X.png");
+  
+  // Y-axis
+  myc->Clear();
+  hy->Draw();
+  myc->Draw();
+  myc->SaveAs(pname+"_1_Y.png");
+  
+  // Z-axis
+  myc->Clear();
+  hz->Draw();
+  myc->Draw();
+  myc->SaveAs(pname+"_1_Z.png");
+  
+  // Fitting
+  // ================
+
+  // create a 3d binned data structure
+  ROOT::Fit::BinData data(nbinsx*nbinsy*nbinsz, nd);
+  double xx[3];
+  for(int ix=1; ix < nbinsx+1; ix++) {
+    float vx = h_s->GetXaxis()->GetBinCenter(ix);
+    xx[0] = vx;
+    for(int iy=1; iy < nbinsy+1; iy++) {
+      float vy = h_s->GetYaxis()->GetBinCenter(iy);
+      xx[1] = vy;
+      for(int iz=1; iz < nbinsz+1; iz++) {
+        float vz = h_s->GetZaxis()->GetBinCenter(iz);
+        float v = h_s->GetBinContent(ix, iy, iz);
+        float ev = h_s->GetBinError(ix, iy, iz);
+        xx[2] = vz;
+        // add the 3d-data coordinate, the predictor value and its errors
+        data.Add(xx, v, ev);
+      }
+    }
+  }
+
+  ROOT::Fit::Fitter fitter;
+  ROOT::Math::WrappedMultiTF1 wf(*g_s,3);
+  fitter.SetFunction(wf);
+
+  std::cout << "DefaultMinimizerAlgo: " << ROOT::Math::MinimizerOptions::DefaultMinimizerAlgo() << std::endl;
+  std::cout << "DefaultMinimizerType: " << ROOT::Math::MinimizerOptions::DefaultMinimizerType() << std::endl;
+  std::cout << "Precision: " << ROOT::Math::MinimizerOptions::DefaultPrecision() << std::endl;
+  for(int ip=0; ip < nd; ip ++ ) {
+    std::cout << "Step for ipar " << ip << " " << g_s->GetParError(ip) << std::endl;
+  }
+
+  ROOT::Fit::FitConfig& fitConfig = fitter.Config();
+  std::vector<ROOT::Fit::ParameterSettings>& parSettings = fitConfig.ParamsSettings();
+  for(int ip=0; ip < parSettings.size(); ip++) {
+    std::cout << "Parameter: " << parSettings[ip].Name() << " Value: " << parSettings[ip].Value() << " Lower: " << parSettings[ip].LowerLimit() << " Upper: " << parSettings[ip].UpperLimit() << " StepSize: " << parSettings[ip].StepSize() << std::endl;
+  }
+
+  // ret = fitter.Fit(data)
+  bool ret = fitter.LikelihoodFit(data);
+  const ROOT::Fit::FitResult &res = fitter.Result();
+  res.Print(std::cout);
+
+  myc->Clear();
+
+  myc->Clear();
+  h_s->Draw();
+
+  // Likelihood fit
+  // h_s->Fit("gaus_s", "L");
+  // Least-square fit
+  // h_s->Fit("gaus_s");
+
+  g_s->SetFitResult(res);
+  // test fit p-value (chi2 probability)
+  double prob = res.Prob();
+  std::cout <<" Chi2 p-value: " << prob << std::endl;
+  
+  myc->Draw();
+  myc->SaveAs(pname+"_2.png");
+  
+  // Check the fit results, by converting TF3 to TH3
+  g_s->SetNpx(nbinsx);
+  g_s->SetNpy(nbinsy);
+  g_s->SetNpz(nbinsz);
+  int npx = g_s->GetNpx();
+  int npy = g_s->GetNpy();
+  int npz = g_s->GetNpz();
+  std::cout << "npx: " << npx << " npy: " << npy << " npz: " << npz << std::endl;
+
+  TH3F *hf = (TH3F*)g_s->CreateHistogram();
+
+  for(int ix=1; ix < npx+1; ix++) {
+    float vx = hf->GetXaxis()->GetBinCenter(ix);
+    for(int iy=1; iy < npy+1; iy++) {
+      float vy = hf->GetYaxis()->GetBinCenter(iy);
+      for(int iz=1; iz < npz+1; iz++) {
+        float vz = hf->GetZaxis()->GetBinCenter(iz);
+        float vf = g_s->Eval(vx, vy, vz);
+        hf->SetBinContent(ix, iy, iz, vf);
+      }
+    }
+  }
+      
+  hf->Print();
+  
+  // Projection
+  TH1F *hfx = (TH1F*)hf->ProjectionX();
+  TH1F *hfy = (TH1F*)hf->ProjectionY();
+  TH1F *hfz = (TH1F*)hf->ProjectionZ();
+  
+  // X-axis
+  myc->Clear();
+  // Normalization
+  float intx = hx->Integral();
+  hfx->Scale(intx/hfx->Integral());
+
+  float ym0 = hx->GetMinimum();
+  float ym1 = hx->GetMaximum();
+  float ym2 = hfx->GetMaximum();
+  if(ym1<ym2) ym1=ym2;
+  hx->GetYaxis()->SetRangeUser(ym0, ym1*1.2);
+
+  hx->Draw();
+  hfx->SetLineColor(2);
+  hfx->Draw("same");
+
+  TLegend *lg = new TLegend(0.6, 0.7, 0.9, 0.9);
+  lg->SetBorderSize(0);
+  lg->SetFillStyle(0);
+  lg->SetTextFont(42);
+  lg->SetTextSize(0.04);
+  lg->AddEntry(hx, "X1", "l");
+  lg->AddEntry(hfx, "3D fit projection X", "l");
+  lg->Draw();
+
+  myc->Draw();
+  myc->SaveAs(pname+"_1_X_fit.png");
+  
+  // Y-axis
+  myc->Clear();
+  // Normalization
+  float inty = hy->Integral();
+  hfy->Scale(inty/hfy->Integral());
+
+  ym0 = hy->GetMinimum();
+  ym1 = hy->GetMaximum();
+  ym2 = hfy->GetMaximum();
+  if(ym1<ym2) ym1=ym2;
+  hy->GetYaxis()->SetRangeUser(ym0, ym1*1.2);
+
+  hy->Draw();
+  hfy->SetLineColor(2);
+  hfy->Draw("same");
+
+  lg->Clear();
+  lg->AddEntry(hy, "X2", "l");
+  lg->AddEntry(hfy, "3D fit projection Y", "l");
+  lg->Draw();
+
+  myc->Draw();
+  myc->SaveAs(pname+"_1_Y_fit.png");
+  
+  // Z-axis
+  myc->Clear();
+  // Normalization
+  float intz = hz->Integral();
+  hfz->Scale(intz/hfz->Integral());
+
+  ym0 = hz->GetMinimum();
+  ym1 = hz->GetMaximum();
+  ym2 = hfz->GetMaximum();
+  if(ym1<ym2) ym1=ym2;
+  hz->GetYaxis()->SetRangeUser(ym0, ym1*1.2);
+
+  hz->Draw();
+  hfz->SetLineColor(2);
+  hfz->Draw("same");
+
+  lg->Clear();
+  lg->AddEntry(hz, "X3", "l");
+  lg->AddEntry(hfz, "3D fit projection Z", "l");
+  lg->Draw();
+
+  myc->Draw();
+  myc->SaveAs(pname+"_1_Z_fit.png");
+
+  // Save files
+  tfout->cd();
+  h_s->Write();
+  hx->Write();
+  hy->Write();
+  hz->Write();
+  g_s->Write();
+  hf->Write();
+  hfx->Write();
+  hfy->Write();
+  hfz->Write();
+  tfout->Close();
+  
+}
---
+++ Fitting/rpc_reso_fit3.py
@@ -0,0 +1,227 @@
+## \file
+## \ingroup tutorial_tmva
+## \notebook
+## Gaussian classifier
+##
+## \macro_image
+## \macro_output
+## \macro_code
+##
+## \author Lailin XU
+
+import os
+from math import sqrt, pow, fabs, log, cos, sin, pi, atan, exp
+import array
+import ROOT as R
+R.gROOT.SetStyle("ATLAS")
+R.gStyle.SetPalette(1)
+
+pname = "gaus_3D_test3"
+tfout = R.TFile("{0}.root".format(pname), "RECREATE")
+
+# Helper function to print a matrx(m, n)
+def print_matrix(m_cov, md, nd):
+
+  for j in range(md):
+    for k in range(nd):
+      print("{0} {1}: {2}".format(j, k, R.TMatrixDRow(m_cov, j)[k]))
+
+
+# Plotting
+# =====================
+myc = R.TCanvas("c", "c", 800, 600)
+myc.SetFillColor(0)
+
+nbinsx, xmin, xmax = 6, -3, 3
+nbinsy, ymin, ymax = 6, -3, 3
+nbinsz, zmin, zmax = 6, -3, 3
+
+# Overlay the multi-Gaussian PDF
+
+# Helper function for 3D Gaussian
+def gaus_3d(x, par):
+
+  if len(x)<3 or len(par)<3: 
+    print("[gaus_3d]: requirs 3D data, and 3 parameters")
+    return 0
+
+  # 3D Gaussian
+  nd = 3
+
+  # Mean
+  # x1-x2, x1-x3, x2-x3
+  mean = [par[0], par[1], par[2]]
+
+  # Covariance matrix 
+  #  sig1^2+sig2^2, sig1^2, sig2^2
+  #  sig1^2, sig1^2+sig3^2, sig3^2
+  #  sig2^2, sig3^2, sig2^2+sig3^2
+  cov = R.TMatrixD(nd, nd)
+  R.TMatrixDRow(cov, 0)[0] = pow(par[3], 2) + pow(par[4], 2)
+  R.TMatrixDRow(cov, 0)[1] = pow(par[3], 2)
+  R.TMatrixDRow(cov, 0)[2] = pow(par[4], 2)
+  R.TMatrixDRow(cov, 1)[0] = pow(par[3], 2)
+  R.TMatrixDRow(cov, 1)[1] = pow(par[3], 2) + pow(par[5], 2)
+  R.TMatrixDRow(cov, 1)[2] = pow(par[5], 2)
+  R.TMatrixDRow(cov, 2)[0] = pow(par[4], 2)
+  R.TMatrixDRow(cov, 2)[1] = pow(par[5], 2)
+  R.TMatrixDRow(cov, 2)[2] = pow(par[4], 2) + pow(par[5], 2)
+
+  # Build the multi-Gaussian
+  m_x = R.TMatrixD(nd, 1)
+  for i in range(nd):
+    R.TMatrixDRow(m_x, i)[0] = x[i] - mean[i]
+
+  m_x_t = R.TMatrixD(m_x)
+  m_x_t.T()
+
+  cov_d = 0
+  icov = R.TMatrixD(cov)
+  icov.Invert(cov_d)
+  cov_d = cov.Determinant()
+  icov_d = icov.Determinant()
+
+  r = 0
+  # r = (m_x_t * icov) * m_x
+  for i in range(nd):
+    for j in range(nd):
+      r += R.TMatrixDRow(m_x_t, 0)[j] * R.TMatrixDRow(icov, i)[j] * R.TMatrixDRow(m_x, i)[0]
+
+  y = 1./(pow(2*pi, nd/2.) * sqrt(cov_d)) * exp( -0.5* r ) 
+
+  return y
+
+# Fit
+npar = 6 # mean: 3; sigma: 3
+g_s = R.TF3("gaus_s", gaus_3d, xmin, xmax, ymin, ymax, zmin, zmax, npar)
+g_s.SetParameter(0, 0.)
+g_s.SetParameter(1, 0.)
+g_s.SetParameter(2, 0.)
+g_s.SetParameter(3, 1.)
+g_s.SetParameter(4, 0.8)
+g_s.SetParameter(5, 1.2)
+
+hname = "3D"
+h_s = R.TH3F(hname, hname, nbinsx, xmin, xmax, nbinsy, ymin, ymax, nbinsz, zmin, zmax)
+h_s.FillRandom("gaus_s", 100)  
+
+h_s.GetXaxis().SetRangeUser(-3, 3)
+h_s.GetYaxis().SetRangeUser(-3, 3)
+h_s.GetZaxis().SetRangeUser(-3, 3)
+h_s.GetXaxis().SetTitle("X1")
+h_s.GetYaxis().SetTitle("X2")
+h_s.GetZaxis().SetTitle("X3")
+
+h_s.Draw("colz")
+
+myc.Draw()
+myc.SaveAs("{0}_1.png".format(pname))
+
+# Projection
+hx = h_s.ProjectionX()
+hy = h_s.ProjectionY()
+hz = h_s.ProjectionZ()
+
+# X-axis
+myc.Clear()
+hx.Draw()
+myc.Draw()
+myc.SaveAs("{0}_1_X.png".format(pname))
+
+# Y-axis
+hy.Draw()
+myc.Draw()
+myc.SaveAs("{0}_1_Y.png".format(pname))
+
+# Z-axis
+hz.Draw()
+myc.Draw()
+myc.SaveAs("{0}_1_Z.png".format(pname))
+
+# Fitting
+print("DefaultMinimizerAlgo: ", R.Math.MinimizerOptions.DefaultMinimizerAlgo())
+print("DefaultMinimizerType: ", R.Math.MinimizerOptions.DefaultMinimizerType())
+
+np = nbinsx*nbinsy*nbinsz
+data = R.Fit.BinData(np, 3)
+xx = array.array("d", [0]*3)
+for ix in range(1, nbinsx+1):
+  for iy in range(1, nbinsy+1):
+    for iz in range(1, nbinsz+1):
+      xx[0] = h_s.GetXaxis().GetBinCenter(ix)
+      xx[1] = h_s.GetYaxis().GetBinCenter(iy)
+      xx[2] = h_s.GetZaxis().GetBinCenter(iz)
+      v = h_s.GetBinContent(ix, iy, iz)
+      ev = h_s.GetBinError(ix, iy, iz)
+      data.Add(xx, v, ev)
+
+fitter = R.Fit.Fitter()
+wf = R.Math.WrappedMultiTF1(g_s, 3)
+fitter.SetFunction(wf)
+#ret = fitter.Fit(data)
+ret = fitter.LikelihoodFit(data)
+res = fitter.Result()
+res.Print(R.cout)
+
+myc.Clear()
+
+h_s.Draw()
+# Likelihood fit
+# h_s.Fit("gaus_s", "L")
+#h_s.Fit("gaus_s")
+
+myc.Draw()
+myc.SaveAs("{0}_2.png".format(pname))
+
+# Check the fit results, by converting TF3 to TH3
+hf = g_s.CreateHistogram()
+npx = g_s.GetNpx()
+npy = g_s.GetNpy()
+npz = g_s.GetNpz()
+for ix in range(1, npx+1):
+  vx = hf.GetXaxis().GetBinCenter(ix)
+  for iy in range(1, npy+1):
+    vy = hf.GetYaxis().GetBinCenter(iy)
+    for iz in range(1, npz+1):
+      vz = hf.GetZaxis().GetBinCenter(iz)
+      vf = g_s.Eval(vx, vy, vz)
+      hf.SetBinContent(ix, iy, iz, vf)
+      
+hf.Print()
+
+# Projection
+hfx = hf.ProjectionX()
+hfy = hf.ProjectionY()
+hfz = hf.ProjectionZ()
+
+# X-axis
+myc.Clear()
+hx.Rebin()
+hx.Draw()
+hfx.SetLineColor(2)
+hfx.Draw("same")
+myc.Draw()
+myc.SaveAs("{0}_1_X_fit.png".format(pname))
+
+# Y-axis
+hy.Rebin()
+hy.Draw()
+hfy.SetLineColor(2)
+hfy.Draw("same")
+myc.Draw()
+myc.SaveAs("{0}_1_Y_fit.png".format(pname))
+
+# Z-axis
+hz.Rebin()
+hz.Draw()
+hfz.SetLineColor(2)
+hfz.Draw("same")
+myc.Draw()
+myc.SaveAs("{0}_1_Z_fit.png".format(pname))
+
+# Save files
+tfout.cd()
+h_s.Write()
+g_s.Write()
+hf.Write()
+tfout.Close()
